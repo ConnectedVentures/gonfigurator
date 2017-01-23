010f028448084a2ac31487c59480542ad4da2cc1
Jim Hill <jimi2204@googlemail.com>
2017-01-23 11:39:20 +0000

    Adding new loader struct, adding tests

--- example_test.go
+++ example_test.go
@@ -1,6 +1,7 @@
 package gonfigurator
 
 import (
+	"errors"
 	"log"
 	"testing"
 )
@@ -26,4 +27,42 @@ func TestTest(t *testing.T) {
 		log.Fatal(err)
 	}
 	log.Println(config.Cloud.ProjectID)
+}
+
+func TestLoaderParse(t *testing.T) {
+	var cfg myserviceConfig
+	l := &Loader{
+		ConfigPath: "./example.yaml",
+	}
+
+	err := l.Parse("./example.yaml", &cfg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if cfg.Logging.Level != "debug" {
+		t.Error("expected logging.level \"debug\", instead found: " + cfg.Logging.Level)
+	}
+}
+
+func TestMockLoader(t *testing.T) {
+	mockedLoader := &LoaderBlueprintMock{
+		ParseFunc: func(path string, target interface{}) error {
+			return errors.New("something went wrong")
+		},
+	}
+	var someTarget interface{}
+	err := mockedLoader.Parse("some path", someTarget)
+	if err == nil {
+		t.Error("expected error, found nil")
+	}
+
+	mockedLoader.ParseFunc = func(path string, target interface{}) error {
+		return nil
+	}
+
+	err = mockedLoader.Parse("some path", someTarget)
+	if err != nil {
+		t.Error(err)
+	}
 }
--- gonfigurator.go
+++ gonfigurator.go
@@ -8,8 +8,13 @@ import (
 	"github.com/ghodss/yaml"
 )
 
-// Loader provides a mockable interface for the gonfigurator library
-type Loader interface {
+// Loader implements the library blueprint
+type Loader struct {
+	ConfigPath string
+}
+
+// LoaderBlueprint provides a mockable interface for the gonfigurator library
+type LoaderBlueprint interface {
 	Parse(string, interface{}) error
 }
 
@@ -19,6 +24,27 @@ func Parse(defaultPath string, v interface{}) error {
 	flag.Parse()
 
 	contents, err := ioutil.ReadFile(*f)
+	if err != nil {
+		return fmt.Errorf("Could not read config file: %s", err.Error())
+	}
+	err = yaml.Unmarshal([]byte(contents), v)
+	if err != nil {
+		return fmt.Errorf("Could not parse config file: %s", err.Error())
+	}
+	return nil
+}
+
+// Parse loads the .yml file from Loader.ConfigPath if set, defaultPath as a fallback.
+// Adheres to the LoaderBlueprint interface.
+func (l *Loader) Parse(defaultPath string, v interface{}) error {
+	cfgPath := l.ConfigPath
+	if l.ConfigPath == "" {
+		f := flag.String("c", defaultPath, "Path to the configuration file")
+		flag.Parse()
+		cfgPath = *f
+	}
+
+	contents, err := ioutil.ReadFile(cfgPath)
 	if err != nil {
 		return fmt.Errorf("Could not read config file: %s", err.Error())
 	}
